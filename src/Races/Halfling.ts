c4f16bebc77f1f5bcbe46049179f2e68efe7512e
juan victor <juanvictor70@gmail.com>
2022-06-06 15:33:16 -0400

    implements Halfling and Orc

---
+++ src/Races/Halfling.ts
@@ -0,0 +1,18 @@
+import Race from './race';
+
+export default class Halfling extends Race {
+  maxLifePoints: number;
+  private static halflingInstances = 0;
+
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+
+    this.maxLifePoints = 60;
+  }
+
+  static createdRacesInstances(): number {
+    Halfling.halflingInstances += 1;
+
+    return Halfling.halflingInstances;
+  }
+}
---
+++ src/Races/Orc.ts
@@ -0,0 +1,18 @@
+import Race from './race';
+
+export default class Orc extends Race {
+  maxLifePoints: number;
+  private static orcInstances = 0;
+
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+
+    this.maxLifePoints = 74;
+  }
+
+  static createdRacesInstances(): number {
+    Orc.orcInstances += 1;
+
+    return Orc.orcInstances;
+  }
+}

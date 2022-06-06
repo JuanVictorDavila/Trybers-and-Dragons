5e7ca779ef3769445710fc04c7ee34d189eb6791
juan victor <juanvictor70@gmail.com>
2022-06-06 15:14:45 -0400

    creating Dwarf, Elf, Halfling, Orc and implements Dwarf and Elf

---
+++ src/Races/Dwarf.ts
@@ -0,0 +1,18 @@
+import Race from './race';
+
+export default class Dwarf extends Race {
+  maxLifePoints: number;
+  private static dwarfInstances = 0;
+
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+
+    this.maxLifePoints = 80;
+  }
+
+  static createdRacesInstances(): number {
+    Dwarf.dwarfInstances += 1;
+
+    return Dwarf.dwarfInstances;
+  }
+}
---
+++ src/Races/Elf.ts
@@ -0,0 +1,18 @@
+import Race from './race';
+
+export default class Elf extends Race {
+  maxLifePoints: number;
+  private static elfInstances = 0;
+
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+
+    this.maxLifePoints = 99;
+  }
+
+  static createdRacesInstances(): number {
+    Elf.elfInstances += 1;
+
+    return Elf.elfInstances;
+  }
+}

e4617d69c3565fef006ec009e45be4104a572d95
juan victor <juanvictor70@gmail.com>
2022-06-06 23:17:41 -0400

    implements the archetypes

--- src/Archetypes/Warrior.ts
+++ src/Archetypes/Warrior.ts
@@ -1,18 +1,18 @@
 import Archetype from './Archetype';
 import { EnergyType } from '../Energy';
 
-export default class Ranger extends Archetype {
+export default class Warrior extends Archetype {
   energyType: EnergyType = 'stamina';
-  private static rangerInstances = 0;
+  private static warriorInstances = 0;
 
-  constructor(name:string) {
+  constructor(name: string) {
     super(name);
     this.energyType = 'stamina';
   }
 
   static createdArchetypeInstances(): number {
-    Ranger.rangerInstances += 1;
+    Warrior.warriorInstances += 1;
 
-    return Ranger.rangerInstances;
+    return Warrior.warriorInstances;
   }
 }

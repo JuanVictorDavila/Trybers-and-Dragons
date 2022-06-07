54b26adc28989c8db8354747c3b4d4273a2d0076
juan victor <juanvictor70@gmail.com>
2022-06-06 23:03:34 -0400

    creating archetype Ranger

---
+++ src/Archetypes/Ranger.ts
@@ -0,0 +1,18 @@
+import Archetype from './Archetype';
+import { EnergyType } from '../Energy';
+
+export default class Ranger extends Archetype {
+  energyType: EnergyType = 'mana';
+  private static rangerInstances = 0;
+
+  constructor(name:string) {
+    super(name);
+    this.energyType = 'stamina';
+  }
+
+  static createdArchetypeInstances(): number {
+    Ranger.rangerInstances += 1;
+
+    return Ranger.rangerInstances;
+  }
+}
